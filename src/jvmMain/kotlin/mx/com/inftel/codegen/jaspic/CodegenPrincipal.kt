8dce1fcbb49bf68226987b21844a22584e399bee
Santos Zatarain Vera <santoszv@inftel.com.mx>
2020-12-18 09:02:20 -0600

    Modificación a "CodegenPrincipal".

--- src/jvmMain/kotlin/mx/com/inftel/codegen/jaspic/CodegenPrincipal.kt
+++ src/jvmMain/kotlin/mx/com/inftel/codegen/jaspic/CodegenPrincipal.kt
@@ -3,13 +3,19 @@ package mx.com.inftel.codegen.jaspic
 import java.security.Principal
 import java.util.*
 
-class CodegenPrincipal(val token: UUID, val uuid: UUID, val id: Long, val username: String, val superuser: Boolean) : Principal {
+class CodegenPrincipal(val token: UUID, val username: String, val superuser: Boolean, val id: Long?, val uuid: UUID?) : Principal {
+
+    constructor(token: UUID, username: String, superuser: Boolean) : this(token, username, superuser, null, null)
+
+    constructor(token: UUID, username: String, superuser: Boolean, id: Long) : this(token, username, superuser, id, null)
+
+    constructor(token: UUID, username: String, superuser: Boolean, uuid: UUID) : this(token, username, superuser, null, uuid)
 
     override fun getName(): String {
         return username
     }
 
     override fun toString(): String {
-        return "CodegenPrincipal(token=$token, uuid=$uuid, id=$id, username='$username', superuser=$superuser)"
+        return "CodegenPrincipal(token=$token, username='$username', superuser=$superuser, id=$id, uuid=$uuid)"
     }
 }
