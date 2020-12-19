82e63a64d3d1d41e8e81199e101c81a3676edba6
Santos Zatarain Vera <santoszv@inftel.com.mx>
2020-12-19 14:28:59 -0600

    Adición de cabeceras.

--- src/commonMain/kotlin/mx/com/inftel/codegen/CodegenConstants.kt
+++ src/commonMain/kotlin/mx/com/inftel/codegen/CodegenConstants.kt
@@ -5,8 +5,11 @@ package mx.com.inftel.codegen
 
 import kotlin.jvm.JvmName
 
+const val ACCEPT_HEADER = "Accept"
+const val ACCEPT_LANGUAGE_HEADER = "Accept-Language"
 const val ANTI_REPLAY_TOKEN_HEADER = "X-Anti-Replay-Token"
 const val AUTHENTICATION_TOKEN_HEADER = "X-Authentication-Token"
+const val CONTENT_TYPE_HEADER = "Content-Type"
 const val EXCEPTION_NAME_HEADER = "X-Exception-Name"
 const val FORWARDED_FOR_HEADER = "X-Forwarded-For"
 
