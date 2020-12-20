5ef3d23493ec4cf9b6a8e50ba3c034e8f278cbed
Santos Zatarain Vera <santoszv@inftel.com.mx>
2020-12-20 11:14:40 -0600

    Adición de "asElementList()" para el tipo "ItemArrayLike<Node>".

--- src/jsMain/kotlin/mx/com/inftel/codegen/browser/BrowserExtensions.kt
+++ src/jsMain/kotlin/mx/com/inftel/codegen/browser/BrowserExtensions.kt
@@ -3,6 +3,8 @@
 package mx.com.inftel.codegen.browser
 
 import org.w3c.dom.Element
+import org.w3c.dom.ItemArrayLike
+import org.w3c.dom.Node
 import org.w3c.xhr.XMLHttpRequest
 import kotlin.coroutines.resume
 import kotlin.coroutines.suspendCoroutine
@@ -12,6 +14,16 @@ import kotlin.coroutines.suspendCoroutine
  */
 val ElementWrapper.wrappedElement: Element
     get() = ElementWrapper.getWrappedElement(this)
+
+fun ItemArrayLike<Node>.asElementList(): List<Element> = object : AbstractList<Element>() {
+
+    override val size: Int get() = this@asElementList.length
+
+    override fun get(index: Int): Element = when (index) {
+        in 0..lastIndex -> this@asElementList.item(index).unsafeCast<Element>()
+        else -> throw IndexOutOfBoundsException("index $index is not in range [0..$lastIndex]")
+    }
+}
 
 /**
  * Make a request with co-routines
