f655d2e6a2f227880a4dfb8e35bb8c31b843cba5
Santos Zatarain Vera <santoszv@inftel.com.mx>
2021-02-08 16:17:51 -0600

    Limpieza de 'CodegenPrincipal'.

--- src/jvmMain/kotlin/mx/com/inftel/codegen/jaspic/CodegenPrincipal.kt
+++ src/jvmMain/kotlin/mx/com/inftel/codegen/jaspic/CodegenPrincipal.kt
@@ -5,13 +5,17 @@ package mx.com.inftel.codegen.jaspic
 import java.security.Principal
 import java.util.*
 
-class CodegenPrincipal(val authToken: UUID, val username: String, val isSuperuser: Boolean, val userId: Long = 0, val userUuid: UUID = UUID(0, 0)) : Principal {
+class CodegenPrincipal(val authToken: UUID, val userid: Long, val username: String, val isSuperuser: Boolean) : Principal {
+
+    init {
+        require(username.isNotBlank())
+    }
 
     override fun getName(): String {
         return username
     }
 
     override fun toString(): String {
-        return "CodegenPrincipal(authToken=$authToken, username='$username', isSuperuser=$isSuperuser, userId=$userId, userUuid=$userUuid)"
+        return "CodegenPrincipal(authToken=$authToken, userid=$userid, username='$username', isSuperuser=$isSuperuser)"
     }
 }
